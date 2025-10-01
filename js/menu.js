fca37d371175b4f6bdfbddb07bcf368bb299152b
Marissa Rico <marissa.rico28@gmail.com>
2025-10-01 13:27:48 +0200

    fixed back canvas menu for panda

--- js/menu.js
+++ js/menu.js
@@ -9,45 +9,48 @@ function drawBackground() {
 
   ctx.strokeStyle = "#333";
   ctx.lineWidth = 8;
-  ctx.strokeRect (0, 0, canvas.width, canvas.height);
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
 const pandaImg = new Image();
 pandaImg.src = "assets/images/panda1.png";
 
 function drawPanda(x, y) {
-  const size = canvas.width *0.15;
+  const size = canvas.width * 0.15;
   ctx.drawImage(pandaImg, x - size / 2, y - size / 2, size, size);
 }
 
 function drawTitle() {
-  ctx.fillStyle = "black"
+  ctx.fillStyle = "black";
   ctx.font = "bold 28px monospace";
   ctx.textAlign = "center";
-  ctx.fillText("🐼 Panda Jump 🐼", canvas.width /2, canvas.height * 0.25);
+  ctx.fillText("🐼 Panda Jump 🐼", canvas.width / 2, canvas.height * 0.25);
 
-  ctx.font = "16px monospace"
-  ctx.fillText("Press space or click to Start", canvas.width /2, canvas.height * 0.35);
+  ctx.font = "16px monospace";
+  ctx.fillText(
+    "Press space or click to Start",
+    canvas.width / 2,
+    canvas.height * 0.35
+  );
 }
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBackground();
   drawTitle();
-  drawPanda(canvas.width / 2, canvas.height *0.9);
-  
+  drawPanda(canvas.width / 2, canvas.height * 0.75);
+
   requestAnimationFrame(gameLoop);
 }
 
 document.addEventListener("keydown", (e) => {
-  if(e.code === "Space") {
-    window.location.href = "pages/level1.html"; 
+  if (e.code === "Space") {
+    window.location.href = "pages/level1.html";
   }
-})
+});
 
 canvas.addEventListener("click", () => {
   window.location.href = "pages/level1.html";
-})
-
+});
 
 gameLoop();
